ce79b6135a45d456338c30a976df0ee453a14cb1
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2024-07-30 13:58:11 -0400

    added button and modal

--- app/page.js
+++ app/page.js
@@ -1,11 +1,35 @@
 "use client"
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
 import { firestore } from "@/firebase";
 import { collection, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  // Pantry list
   const [pantry, setPantry] = useState([])
+
+  // Modal
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  // Adding an item
+  const [itemName, setItemName] = useState('')
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1.5
+  };
 
   useEffect(() => {
     console.log('updating pantry')
@@ -22,6 +46,11 @@ export default function Home() {
     updatePantry()
   }, []);
 
+  // Adding an item to the pantry
+  const addItem = async (item) => {
+    console.log(item)
+  }
+
   return (
     <Box 
       width="100vw"
@@ -30,7 +59,41 @@ export default function Home() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
+      gap={1.5}
     >
+      <Modal 
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width='100%' direction={'row'} spacing={2} justifyContent='center'>
+            <TextField 
+              id="outlined-basic" 
+              label="Item" 
+              variant="outlined" 
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button 
+              variant="contained" 
+              onClick={ () => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>
+        Add Item
+      </Button>
       <Box border={'1px solid #333'}>
         <Box sx={{
           width: '800px',
