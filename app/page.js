5ec73d219017924ca578116b8fc9ab645470639d
Mazen Ibrahim <111891348+MazenIbrahim1@users.noreply.github.com>
2024-07-31 00:39:06 -0400

    Adding and removing functionality and quantity

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
 import { firestore } from "@/firebase";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, getDocs, getDoc, query, doc, setDoc, deleteDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -16,6 +16,7 @@ export default function Home() {
   // Adding an item
   const [itemName, setItemName] = useState('')
 
+  // Style for modal
   const style = {
     position: 'absolute',
     top: '50%',
@@ -31,24 +32,56 @@ export default function Home() {
     gap: 1.5
   };
 
+  // Get the pantry data
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry')) 
+    const docs = await getDocs(snapshot)
+    const pantryList = []
+    docs.forEach(doc => {
+      pantryList.push({
+        name: doc.id,
+        ...doc.data()
+      })
+    })
+    console.log(pantryList)
+    setPantry(pantryList)
+  }
+
   useEffect(() => {
     console.log('updating pantry')
-    const updatePantry = async () => {
-      const snapshot = query(collection(firestore, 'pantry')) 
-      const docs = await getDocs(snapshot)
-      const pantryList = []
-      docs.forEach(doc => {
-        pantryList.push(doc.id)
-      })
-      console.log(pantryList)
-      setPantry(pantryList)
-    }
     updatePantry()
   }, []);
 
-  // Adding an item to the pantry
+  // Adding an item to the pantry database
   const addItem = async (item) => {
-    console.log(item)
+    console.log('Adding item: ', item)
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    // Check if it exists
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()) {
+      const {count} = docSnap.data()
+      await setDoc(docRef, {count: count + 1})
+    } else {
+      await setDoc(docRef, {count: 1})
+    }
+    await updatePantry()
+  }
+  
+  // Removing an item from the database
+  const removeItem = async (item) => {
+    console.log('Removing: ', item)
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    // Check if it exists
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()) {
+      const {count} = docSnap.data()
+      if(count === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {count: count - 1})
+      }
+    }
+    await updatePantry()
   }
 
   return (
@@ -112,25 +145,30 @@ export default function Home() {
           </Typography>
         </Box>
         <Stack width="800px" height="300px" spacing={2} overflow={'auto'}> 
-          {pantry.map((i) => (
-            <Box 
-            key={i}
-              sx={{
-                width: "100%",
-                minHeight: "150px",
-                display: 'flex',
-                flexDirection: { xs: "column", md: "row"},
-                justifyContent: "center",
-                alignItems: "center",
-                bgcolor: '#f0f0f0'
-              }}
-            >
-              <Typography variant={"h3"} color={'#333'} textAlign={'center'}>
-                {
-                  // Capitalize the first letter
-                  i.charAt(0).toUpperCase() + i.slice(1)
-                }
-              </Typography>
+          {pantry.map(({name, count}) => (
+            <Box
+              key={name}
+                sx={{
+                  width: "100%",
+                  minHeight: "150px",
+                  display: 'flex',
+                  flexDirection: { xs: "column", md: "row"},
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  bgcolor: '#f0f0f0',
+                  paddingX: 4
+                }}
+              >
+                <Typography variant={"h3"} color={'#333'} textAlign={'center'}>
+                  {
+                    // Capitalize the first letter
+                    name.charAt(0).toUpperCase() + name.slice(1)
+                  }
+                </Typography>
+                <Typography variant={"h3"} color={'#333'} textAlign={'center'}>
+                  Quantity: {count}
+                </Typography>
+              <Button variant="contained" onClick={() => removeItem(name)}>Remove</Button>
             </Box>
           ))}
         </Stack>
